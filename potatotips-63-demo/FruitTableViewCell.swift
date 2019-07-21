017a68a8c4722d9944b90541251f2c50b2aba361
uhooi <uhooi@server.fake>
2019-07-21 19:11:01 +0900

    リファクタリング後

--- potatotips-63-demo/FruitTableViewCell.swift
+++ potatotips-63-demo/FruitTableViewCell.swift
@@ -8,19 +8,16 @@
 
 import UIKit
 
-class FruitTableViewCell: UITableViewCell {
+final class FruitTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+    // MARK: IBOutlet
     
-    @IBOutlet weak var fruitLabel: UILabel!
+    @IBOutlet private weak var fruitLabel: UILabel!
+    
+    // MARK: Internal Methods
+    
+    func setup(fruit: String) {
+        self.fruitLabel.text = fruit
+    }
     
 }
--- potatotips-63-demo/ViewController.swift
+++ potatotips-63-demo/ViewController.swift
@@ -8,49 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+final class ViewController: UIViewController, UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fruits.count
-    }
+    // MARK: Properties
     
-    @IBAction func didTapAppleButton(_ sender: UIButton) {
-        addFruits(fruit: "りんご")
-    }
-    
-    @IBAction func didTapOrangeButton(_ sender: UIButton) {
-        addFruits(fruit: "みかん")
-    }
-    
-    func addFruits(fruit: String) {
-        self.fruits.append(fruit)
-    }
-    
-    var fruits: [String] = [] {
+    private var fruits: [String] = [] {
         didSet {
             self.fruitsTableView.reloadData()
         }
     }
     
-    @IBOutlet weak var fruitsTableView: UITableView!
+    // MARK: IBOutlet
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-//        self.fruitsTableView.delegate = self
-        self.fruitsTableView.dataSource = self
-//        self.fruitsTableView.register(UINib(nibName:  "FruitTableViewCell", bundle: nil), forCellReuseIdentifier: "FruitTableViewCell")
+    @IBOutlet private weak var fruitsTableView: UITableView!
+    
+    // MARK: IBAction
+    
+    @IBAction private func didTapAppleButton(_ sender: UIButton) {
+        addFruits(fruit: "りんご")
     }
     
-    @IBAction func didTapBananaButton(_ sender: UIButton) {
+    @IBAction private func didTapOrangeButton(_ sender: UIButton) {
+        addFruits(fruit: "みかん")
+    }
+    
+    @IBAction private func didTapBananaButton(_ sender: UIButton) {
         addFruits(fruit: "ばなな")
+    }
+    
+    // MARK: UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.fruits.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.fruitsTableView.dequeueReusableCell(withIdentifier: "FruitTableViewCell", for: indexPath) as! FruitTableViewCell
-        let fruit = fruits[indexPath.row]
-        cell.fruitLabel.text = fruit
+        cell.setup(fruit: fruits[indexPath.row])
         return cell
     }
 
+    // MARK: Private Methods
+    
+    private func addFruits(fruit: String) {
+        self.fruits.append(fruit)
+    }
+    
 }
